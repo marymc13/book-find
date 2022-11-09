4021a8e8b7ee69677099bd2a261c9d56cb28ec90
Mary Paul <marymc135@hotmail.com>
2022-11-09 09:43:54 -0500

    resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,27 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+      me: async (parent, args) => {
+  
+          const user = await User.create(args)
+  
+          return user;
+        }
+    },
+      Mutation: {
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+
+          return { user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+          return { user };
+        },
       }
-    }
+    
   };
   
   module.exports = resolvers;
