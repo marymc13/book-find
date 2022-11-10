e46f0a4ba2ef9526d9c29bab97cea0403645c335
Mary Paul <marymc135@hotmail.com>
2022-11-09 22:25:34 -0500

    update apollo/client

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,27 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -25,7 +44,8 @@ function App() {
         </Switch>
       </>
     </Router>
-  );
+    </ApolloProvider>
+  ); 
 }
 
 export default App;
