55287439d5651a13790ad104662564e1d762ab8e
Mary Paul <marymc135@hotmail.com>
2022-11-10 22:23:30 -0500

    package.json

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-find', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:127.0.0.1:27017/book-find', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
